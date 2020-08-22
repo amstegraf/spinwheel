10439144dd27cb41144100b8ce41deefe49c9e14
Apostol Aurelian <aurelian.apostol@gmail.com>
2020-08-23 00:14:16 +0300

    added windows bat file and example of run

---
+++ example/start.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+set mypath=%cd%
+ECHO Running draw from following list of names: "%mypath%\names.txt"
+PAUSE
+java -jar -DFILE_PATH="%mypath%\names.txt" spin-wheel-1.0-SNAPSHOT.jar
+PAUSE
---
+++ example/start_with_suspense_delay.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+set mypath=%cd%
+ECHO Running draw from following list of names: "%mypath%\names.txt"
+PAUSE
+java -jar -DFILE_PATH="%mypath%\names.txt" -DSLEEP_ENABLED=TRUE -DSLEEP_PERIOD=1 -DMIN_SHUFFLES=1 -DMAX_SHUFFLES=5 -DMIN_WINNING_DRAWS=1 -DMAX_WINNING_DRAWS=5 spin-wheel-1.0-SNAPSHOT.jar
+PAUSE
